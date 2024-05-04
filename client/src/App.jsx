059fd451a38b09b2d8eddcadcfba21e623c9b18a
hegarciagg <hegarciagg@gmail.com>
2024-05-04 17:42:52 -0500

    Creo paginas y rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import SingUp from "./pages/SignUp";
+
 export default function App() {
-  return <h1 className="text-3xl">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signUp" element={<SingUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
