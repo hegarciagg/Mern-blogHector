fb9a8ba72c3deff873c266733de1d640354cd266
hegarciagg <hegarciagg@gmail.com>
2024-05-18 10:43:36 -0500

    add create a post api rout

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -10,21 +11,23 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("MongoDb is connected");
+    console.log("Mongodb is connected");
   })
   .catch((err) => {
     console.log(err);
   });
-const app = express();
 
+const app = express();
 app.use(express.json());
-app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log("server listening on port 3000!!!");
 });
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
