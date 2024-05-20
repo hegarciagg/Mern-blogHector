2665b79b91ad657e654b72b3bec80ae577449cc7
hegarciagg <hegarciagg@gmail.com>
2024-05-19 19:12:01 -0500

    show the comments of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUsers,
   signout,
   test,
@@ -9,10 +10,12 @@ import {
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
+
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
